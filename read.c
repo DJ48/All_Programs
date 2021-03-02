268d0cf94e0409b8a782832d4cd328552948b0ec
Deepak Jaiswal <35431078+DJ48@users.noreply.github.com>
2021-03-02 12:27:53 +0530

    Add files via upload

---
+++ read.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Employee {
+   char  fname[20],lname[20];
+   char  id[5];
+   char company[10];
+};
+
+int main ()
+{
+   FILE *infile;
+   struct Employee input;
+
+   /*** open the accounts file ***/
+   infile = fopen ("EmployeeDB.dat","r");
+   if (infile == NULL)
+     {
+      fprintf(stderr, "\nError opening accounts.dat\n\n");
+      exit (1);
+     }
+
+   while (fread (&input, sizeof(struct Employee), 1, infile))
+      printf ("Name = %10s %10s   ID = %s   Company = %s\n",
+              input.fname, input.lname, input.id, input.company);
+   return 0;
+}
---
+++ write.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// a structure to read and write
+struct Employee {
+   char  fname[20],lname[20];
+   char  id[5];
+   char company[10];
+};
+
+/**************************************/
+int main ()
+{
+   FILE *outfile;
+   struct Employee input;
+
+   // open Accounts file for writing
+   outfile = fopen ("EmployeeDB.dat","wb");
+   if (outfile == NULL)
+     {
+      fprintf(stderr, "\nError opening accounts.dat\n\n");
+      exit (1);
+     }
+
+   // instructions to user
+   printf("Enter \"stop\" for First Name to end program.");
+
+   // endlessly read from keyboard and write to file
+   while (1)
+     {
+      // prompt user
+      printf("\nFirst Name: ");
+      scanf ("%s", input.fname);
+      // exit if no name provided
+      if (strcmp(input.fname, "stop") == 0)
+         exit(1);
+      // continue reading from keyboard
+      printf("Last Name : ");
+      scanf ("%s", input.lname);
+      printf("ID  : ");
+      scanf ("%s", input.id);
+      printf("Company   : ");
+      scanf ("%s", input.company);
+
+      // write entire structure to Accounts file
+      fwrite (&input, sizeof(struct Employee), 1, outfile);
+     }
+     return 0;
+}
